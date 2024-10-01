2b4084f8097cdef7eb15cc01833f19282bbf78d4
PaulinhoLaniusJunior <150497439+PaulinhoLaniusJunior@users.noreply.github.com>
2024-10-01 13:39:38 -0300

    atualizado

---
+++ TELA_INICIAL/script.js
@@ -0,0 +1,5 @@
+const loginBtn = document.getElementById('loginBtn');
+
+loginBtn.addEventListener('click', () => {
+    window.location.href = 'TELA DE LOGIN/index.html'; // Redireciona para a página de login
+});

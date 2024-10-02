374758d72a5650affc3d2408ce2848e79e58ac68
PaulinhoLaniusJunior <150497439+PaulinhoLaniusJunior@users.noreply.github.com>
2024-10-02 19:46:00 -0300

    Delete TELA_INICIAL directory

--- TELA_INICIAL/script.js
+++
@@ -1,5 +0,0 @@
-const loginBtn = document.getElementById('loginBtn');
-
-loginBtn.addEventListener('click', () => {
-    window.location.href = 'TELA DE LOGIN/index.html'; // Redireciona para a página de login
-});

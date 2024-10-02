989e102f291e6d9c149724cc646dce8106419c82
PaulinhoLaniusJunior <150497439+PaulinhoLaniusJunior@users.noreply.github.com>
2024-10-02 19:46:32 -0300

    NOVA ORGANIZAÇÃO

---
+++ src/home/script.js
@@ -0,0 +1,5 @@
+const loginBtn = document.getElementById('loginBtn');
+
+loginBtn.addEventListener('click', () => {
+    window.location.href = '../login/index.html'; // Redireciona para a página de login
+});

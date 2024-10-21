6eaa0c71ba99f6bd02dfbfdb377fa4e252881add
Borges07 <168027802+Borges07@users.noreply.github.com>
2024-10-21 20:12:43 -0300

    topd++
    teste de commit

--- src/home/script.js
+++
@@ -1,5 +0,0 @@
-const loginBtn = document.getElementById('loginBtn');
-
-loginBtn.addEventListener('click', () => {
-    window.location.href = '../login/index.html'; // Redireciona para a página de login
-});
